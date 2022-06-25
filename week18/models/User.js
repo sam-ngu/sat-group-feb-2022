2f10ed1687fe0c89988fa463cd0f5698ff2355b3
Sam <opensource@acadea.io>
2022-06-25 10:21:07 +0800

    added user model

---
+++ week18/models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [function (email) {
+        var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return re.test(email)
+      }, "Invalid email"]
+    },
+    
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ]
+  },
+  {
+    id: true,
+    timestamps: true,
+    
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
