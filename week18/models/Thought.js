b7d0cfe155acb9f4a7f1b97ea419486e0447f310
Sam <opensource@acadea.io>
2022-06-25 10:36:00 +0800

    added thought model and reaction sub doc

---
+++ week18/models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+
+const reactionSchema = new Schema({
+
+  body: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  }
+
+}, {
+  id: true,
+  timestamps: true,
+});
+
+
+// Schema to create a thought model
+const thoughtSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+
+    },
+
+    reactions: [reactionSchema],
+    
+  },
+  {
+    id: true,
+    timestamps: true,
+
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
